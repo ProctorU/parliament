6303ced7a9d2e7f73daa4e36d5fc60390a7b43f9
Kevin Brown <chevinbrown@gmail.com>
2018-03-15 10:32:11 -0500

    Remove todos

--- lib/parliament/form_helpers.rb
+++ lib/parliament/form_helpers.rb
@@ -1,8 +1,3 @@
-# TODO:
-# 1. Methods add and remove associations should render a partial
-# 2. We should be able to infer the form object and build the association from
-#    the association param
-# 3. Determine the API needed to have flexible association creation
 module Parliament
   class ActionView::Helpers::FormBuilder
     def add_association(link_name, association, options = {}, &block)
